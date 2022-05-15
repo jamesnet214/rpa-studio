079b5ac5b0a4c170d286f32fb111ae1f755e6d05
임화식 <harry.lim@devncore.org>
2022-05-15 20:09:50 +0900

    233

---
+++ src/RoslynTest/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using RoslynPad.Roslyn.CodeActions;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace RoslynTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+    internal sealed class CodeActionsConverter : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((Microsoft.CodeAnalysis.CodeActions.CodeAction)value).GetCodeActions();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
--- src/RpaTestModule/TestClass.cs
+++ src/RpaTestModule/TestClass.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json;
 using System;
 
 namespace RpaTestModule
@@ -7,6 +8,7 @@ namespace RpaTestModule
         public void 테스트()
         {
             Console.WriteLine("테스트");
+            JsonConvert.SerializeObject(new TestClass());
         }
 
         public int 테스트(int a, int b)
