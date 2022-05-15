9635e4ab81f29440c2ac03c6641360844110a0ce
임화식 <harry.lim@devncore.org>
2022-05-15 11:57:03 +0900

    테스트

---
+++ src/RpaStudio/Starter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpaStudio
+{
+    public static class Starter
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            new App().Run();
+        }
+    }
+}
---
+++ src/RpaTestModule/TestClass.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace RpaTestModule
+{
+    public class TestClass
+    {
+        public void 테스트()
+        {
+            Console.WriteLine("테스트");
+        }
+
+        public int 테스트(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
